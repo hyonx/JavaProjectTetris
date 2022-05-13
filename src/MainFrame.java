bf98d2ff0aa8d668bc1d305574306b7ec15c8114
hanmengxuan <104689232+hanmengxuan@users.noreply.github.com>
2022-05-13 12:10:00 +0800

    线程问题

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,21 +1,26 @@
+com.sun.applet2.Applet2;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class MainFrame {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         JFrame frame = new JFrame("玩玩俄罗斯方块");
 
         Tetris gamePanel = new Tetris();
         HomePanel homePanel = new HomePanel();
         GameOverPanel gameOverPanel = new GameOverPanel();
-        PausePanel pausePanel=new PausePanel();
+        PausePanel pausePanel = new PausePanel();
 
         //设置一个窗口的总面板
         JPanel mainPanel = new JPanel();
 
-        //创建一个卡片布局器的对象
+       //创建一个卡片布局器的对象
         CardLayout cardLayout = new CardLayout();
 
 
@@ -30,7 +35,10 @@ public class MainFrame {
         mainPanel.add(gameOverPanel);
         //mainPanel.add(pausePanel);
 
+
+
         frame.setContentPane(mainPanel);
+
 
         frame.setSize(535, 595);
         frame.setLocationRelativeTo(null);
@@ -40,13 +48,25 @@ public class MainFrame {
         //when game_state is "HOME", we need to display the homePanel;
         //when game_state is "PLAYING", we need to display the gamePanel and start the game;
 
+        Thread a=new Thread(gamePanel);
+        Thread b=new Thread(gamePanel);
+        Thread c=new Thread(gamePanel);
+        Thread d=new Thread(gamePanel);
+        Thread e=new Thread(gamePanel);
+        Thread f=new Thread(gamePanel);
+
 
         homePanel.start.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
+                Tetris.exit=true;
                 gamePanel.setGameState(0);
                 cardLayout.next(mainPanel);
+
                 new Thread(gamePanel).start();
+             //   a.start();
+
             }
         });
 
@@ -56,7 +76,13 @@ public class MainFrame {
                 gamePanel.setGameState(0);
                 cardLayout.first(mainPanel);
                 cardLayout.next(mainPanel);
+
+                //Tetris.exit= true;
+               //不能用
+                // Tetris.startInitiation();
+              //  b.start();
                 new Thread(gamePanel).start();
+
             }
         });
 
@@ -65,7 +91,9 @@ public class MainFrame {
             public void actionPerformed(ActionEvent e) {
                 gamePanel.setGameState(3);
                 cardLayout.first(mainPanel);
+                //也许不需要
                 new Thread(gamePanel).start();
+              //  a.start();
             }
         });
 
@@ -74,7 +102,8 @@ public class MainFrame {
             public void actionPerformed(ActionEvent e) {
                 gamePanel.setGameState(3);
                 cardLayout.first(mainPanel);
-                new Thread(gamePanel).start();
+               new Thread(gamePanel).start();
+               // a.start();
             }
         });
 
@@ -83,7 +112,28 @@ public class MainFrame {
             public void actionPerformed(ActionEvent e) {
                 gamePanel.setGameState(0);
                 cardLayout.previous(mainPanel);
+               // Tetris.startInitiation();
+                //a.start();
                 new Thread(gamePanel).start();
+            }
+        });
+
+
+        pausePanel.save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            //    File record=new File("records.txt");
+                PrintWriter printWriter= null;
+                try {
+                    printWriter = new PrintWriter("records.txt");
+                } catch (FileNotFoundException ex) {
+                    ex.printStackTrace();
+                }
+
+                System.out.print(Tetris.getTotalScore());
+                printWriter.print(Tetris.getTotalLine());
+                printWriter.print(Tetris.getTotalScore());
+                System.out.println("save");
             }
         });
 
@@ -92,6 +142,8 @@ public class MainFrame {
             public void actionPerformed(ActionEvent e) {
                 gamePanel.setGameState(1);
                 cardLayout.next(mainPanel);
+               // a.start();
+                new Thread(gamePanel).start();
             }
         });
 
@@ -104,8 +156,15 @@ public class MainFrame {
                     break;
                 }
             }
+            System.out.print("qqqqq");
+            Tetris.exit= false;
+
             cardLayout.last(mainPanel);
+            //不能删
             gamePanel.setGameState(0);
+
         }
     }
-}
+
+
+    }
