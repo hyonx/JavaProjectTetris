8582815ab702a68e4a8c676fee1e208638eafcd6
hanmengxuan <104689232+hanmengxuan@users.noreply.github.com>
2022-05-13 17:17:04 +0800

    home panel

---
+++ src/HomePanel.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class HomePanel extends JPanel {
+    JButton start;
+    JButton load;
+    JButton quit;
+    public HomePanel(){
+        Container a=new Container();
+        start = new JButton("start");
+        load = new JButton("load");
+        quit = new JButton("quit");
+        a.add(start);
+
+      //  start.setBounds(200,200,40,40);
+        this.add(start);
+        this.add(load);
+        this.add(quit);
+        start.setVisible(true);
+        load.setVisible(true);
+        quit.setVisible(true);
+    }
+}
