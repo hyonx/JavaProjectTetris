f9cceda68a7c2219bd659fa002882303122969de
hanmengxuan <104689232+hanmengxuan@users.noreply.github.com>
2022-05-16 00:45:15 +0800

    结束后显示分数

--- src/GameOverPanel.java
+++ src/GameOverPanel.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GameOverPanel extends JPanel{
     JButton restart;
@@ -8,9 +9,18 @@ public class GameOverPanel extends JPanel{
         this.setLayout(null);
         restart=new JButton("RESTART");
         home=new JButton("HOME");
+        restart.setBounds(150,310,200,80);
+        home.setBounds(150,410,200,80);
         this.add(restart);
         this.add(home);
         restart.setVisible(true);
         home.setVisible(true);
+    }
+    public void paint(Graphics g){
+        g.drawImage(Tetris.gameOver, 0, 0, null);
+        g.setFont(new Font(Font.SANS_SERIF, Font.ITALIC, 30));
+        g.drawString("SCORES:" + Tetris.getTotalScore(), 200, 250);
+        g.drawString("LINES:" + Tetris.getTotalLine(), 200, 295);
+        super.paintChildren(g);
     }
 }
