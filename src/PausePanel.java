45cc1d13af202a7b4e2531b2baf21d2c37a022e6
hanmengxuan <104689232+hanmengxuan@users.noreply.github.com>
2022-05-15 19:32:35 +0800

    Update PausePanel.java

--- src/PausePanel.java
+++ src/PausePanel.java
@@ -3,13 +3,23 @@ import javax.swing.*;
 public class PausePanel extends JPanel{
     JButton startAgain;
     JButton homeAgain;
+    JButton save;
+    JButton Continue;
 
     public PausePanel(){
         startAgain=new JButton("RESTART");
         homeAgain=new JButton("HOME");
+        save=new JButton("SAVE");
+        Continue=new JButton("CONTINUE");
         this.add(startAgain);
+        this.add(Continue);
+        this.add(save);
         this.add(homeAgain);
+
+        save.setVisible(true);
+        Continue.setVisible(true);
         startAgain.setVisible(true);
         homeAgain.setVisible(true);
     }
 }
+
