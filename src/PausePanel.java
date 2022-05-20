2c6e3cefa606eb0ee1b806c1545d69228929862a
hanmengxuan <104689232+hanmengxuan@users.noreply.github.com>
2022-05-20 23:22:41 +0800

    5-20 按钮&背景

--- src/PausePanel.java
+++ src/PausePanel.java
@@ -1,4 +1,13 @@
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+
 import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+
 public class PausePanel extends JPanel{
     JButton startAgain;
     JButton homeAgain;
@@ -17,30 +26,34 @@ public class PausePanel extends JPanel{
 
     public PausePanel(){
         this.setLayout(null);
-        startAgain=new JButton("RESTART");
-        homeAgain=new JButton("HOME");
-        save=new JButton("SAVE");
-        Continue=new JButton("CONTINUE");
+        startAgain=new JButton("Restart");
+        homeAgain=new JButton("Home");
+        save=new JButton("Save");
+        Continue=new JButton("Continue");
 
-       
+        startAgain.setBounds(163*2,163*2,200*2,65*2);
+        Continue.setBounds(163*2,252*2,200*2,65*2);
+        save.setBounds(163*2,341*2,200*2,65*2);
+        homeAgain.setBounds(163*2,430*2,200*2,65*2);
 
+        startAgain.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 22*2));
+        Continue.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 22*2));
+        save.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 22*2));
+        homeAgain.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 22*2));
 
-        
-        startAgain.setBounds(150*2,100*2,200*2,80*2);
-        Continue.setBounds(150*2,200*2,200*2,80*2);
-        save.setBounds(150*2,300*2,200*2,80*2);
-        homeAgain.setBounds(150*2,400*2,200*2,80*2);
-        
-        this.add(startAgain);
         this.add(Continue);
+        this.add(startAgain);
         this.add(save);
         this.add(homeAgain);
-        
-
         
         save.setVisible(true);
         Continue.setVisible(true);
         startAgain.setVisible(true);
         homeAgain.setVisible(true);
+    }
+
+    public void paint(Graphics g){
+        g.drawImage(Tetris.Pause, 0, -80, null);
+        super.paintChildren(g);
     }
 }
