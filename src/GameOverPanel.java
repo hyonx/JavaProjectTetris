ca6e6f75770d9345924291b86fa87ec5715d7269
hanmengxuan <104689232+hanmengxuan@users.noreply.github.com>
2022-05-20 23:23:19 +0800

    5-20 按钮&背景

--- src/GameOverPanel.java
+++ src/GameOverPanel.java
@@ -7,20 +7,23 @@ public class GameOverPanel extends JPanel{
 
     public GameOverPanel(){
         this.setLayout(null);
-        restart=new JButton("RESTART");
-        home=new JButton("HOME");
-        restart.setBounds(150*2,310*2,200*2,80*2);
-        home.setBounds(150*2,410*2,200*2,80*2);
+        restart=new JButton("Restart");
+        home=new JButton("Home");
+        restart.setBounds(163*2,310*2,200*2,70*2);
+        home.setBounds(163*2,410*2,200*2,70*2);
+        restart.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 22*2));
+        home.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 22*2));
         this.add(restart);
         this.add(home);
         restart.setVisible(true);
         home.setVisible(true);
     }
     public void paint(Graphics g){
-        g.drawImage(Tetris.gameOver, 0, 0, null);
-        g.setFont(new Font(Font.SANS_SERIF, Font.ITALIC, 30*2));
-        g.drawString("SCORES:" + Tetris.getTotalScore(), 200*2, 250*2);
-        g.drawString("LINES:" + Tetris.getTotalLine(), 200*2, 295*2);
+        g.drawImage(Tetris.gameOver, 0, -150, null);
+        g.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 30*2));
+        g.drawString("SCORES:" + Tetris.getTotalScore(), 50*2, 250*2);
+        g.drawString("LINES:" + Tetris.getTotalLine(), 340*2, 250*2);
         super.paintChildren(g);
     }
 }
+
